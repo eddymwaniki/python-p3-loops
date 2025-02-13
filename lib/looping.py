6b638635c5057c386ecf10b09e7de930e499953c
eddy <eddy.mwaniki.27@gmail.com>
2025-02-13 12:25:26 +0300

    modifies looping.py

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    counter = 10
+    while counter > 0 :
+        print(counter)
+        counter -= 1
+        print("Happy New Year!")
     pass
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+   return [squares ** 2 for squares in int_list]
+   pass
 
 def fizzbuzz():
-    # code goes here!
+    for num in range(1, 101) :
+        if num % 15 == 0:
+            print("FizzBuzz")
+        elif num % 5 == 0:
+            print("Buzz")   
+        elif  num % 3 == 0 :
+           print("Fizz")  
+        else :
+            print(num)
+
     pass
